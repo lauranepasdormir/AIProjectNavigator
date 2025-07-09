f113f4f9e0822b2f441d2c03e130e65f943ef30a
fideris susanto <fsus8677@uni.sydney.edu.au>
2025-07-09 10:56:52 +1000

    added all the evaluation criteria in the evalCriteria.ts file

--- client/src/lib/evalCriteria.ts
+++ client/src/lib/evalCriteria.ts
@@ -7,17 +7,27 @@ export interface Criteria {
 export const evalCriteria: Criteria[] = [
   {
     id: 1,
-    text: "2-10 words in length, indicate purpose or domain, avoids filler words or project codes, only contains letters, numbers, and spaces.",
+    text: "2-10 words in length (do not mention the word limit in your advice)",
     question: "title"
   },
   {
     id: 2,
-    text: "at least 2 sentences, mention the industry type, mention project purpose, functionality and intended effect",
+    text: "at least 2 sentences (do not mention the word limit in your advice), mention a specific industry type, mention project purpose, functionality and intended effect",
     question: "description"
   },
   {
     id: 3,
-    text: "Please provide a brief description of your project (2-3 sentences).",
+    text: "Clear statement of a single, primary problem, no metion of a solution, and contains at least one key end user or stakeholder",
     question: "problem"
+  },
+  {
+    id: 4,
+    text: "Mentions at least one key AI technology, uses recognizable Ai terminology from the AI domain, and justifies the use of AI technology",
+    question: "technology"
+  },
+  {
+    id: 5,
+    text: "States at least one potential impact and its business value, identifies a beneficiary or affected group, relevant to the problem definition, and it states an impact in at least one of the following areas : social, economincal, technological, environmental",
+    question: "impact"
   }
 ];
